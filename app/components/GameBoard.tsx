b9bd87b46da7cb67dc08aa9baace7dd3faefa154
Olga-allgood <olga.s.orlova@gmail.com>
2026-01-22 15:52:20 -0800

    Game Board Component: states and function isComplete()

--- app/components/GameBoard.tsx
+++ app/components/GameBoard.tsx
@@ -1,3 +1,35 @@
-export default function GameBoard({userId}){
+'use client'
+import { useState, useEffect } from 'react';
+import { supabase } from "@/app/lib/supabaseClient";
+import word_with_examples from "@/app/types/database";
+
+
+interface Profile {
+    id: string
+}
+export default function GameBoard({userId}: Profile){
+    const [currentWord, setCurrentWord] = useState<word_with_examples | null>(null);
+    const [guessedLetters, setGuessedLetters] = useState(null)
+    const [incorrectGuesses, setIncorrectGuessed] = useState(0)
+    const [sessionId, setSessionId] = useState(null)
+    const [isCompleted, setIsCompleted] = useState(false)
+    const [isFailed, setIsFailed] = useState(false)
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
+
+    function isComplete(word: string, guessed: string){
+        const word_letters=word.toLowerCase()
+        const guessed_letters=guessed.toLowerCase()
+        if (word_letters == guessed_letters){
+            return true    
+        }
+        return false
+
+    
+        
+
+    }
+
     return (<p>Game Board</p>)
 }
+
--- app/types/database.ts
+++ app/types/database.ts
@@ -3,3 +3,4 @@ export interface profile {id: string, email: string, created_at: string};
 export interface word {id: string, word: string, meaning: string, created_at: string};
 export interface game_session {id: string, user_id: string, word_id: string, status: boolean, correct_guesses: number, incorrect_guesses: number,}
 export interface example {id: string, word_id: string, example_text: string}
+export interface word_with_examples extends word {examples: example[]}
