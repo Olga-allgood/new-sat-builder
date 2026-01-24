e73ca752bd5afe0979938e13513ecd0a405eaf65
Olga-allgood <olga.s.orlova@gmail.com>
2026-01-24 07:36:48 -0800

    GameBoard component updated code

--- app/components/GameBoard.tsx
+++ app/components/GameBoard.tsx
@@ -3,7 +3,6 @@ import { useState, useEffect } from 'react';
 import { supabase } from "@/app/lib/supabaseClient";
 import {word_with_examples} from "@/app/types/database";
 
-
 interface GameBoard {
     id: string
 }
@@ -16,6 +15,7 @@ export default function GameBoard({userId}: GameBoard) {
     const [isFailed, setIsFailed] = useState(false)
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState('')
+
 
     function isComplete(word: string, guessed: Set <string>){
         const word_letters=word.toLowerCase().split("")
@@ -57,6 +57,7 @@ export default function GameBoard({userId}: GameBoard) {
             return
         }
         setCurrentWord({...randomWord, examples:examples || []})
+        const MAX_CUESSES = currentWord.word.length*2
 
 
         // another query for the session
@@ -77,6 +78,9 @@ export default function GameBoard({userId}: GameBoard) {
     await startNewGame();
   })();
 }, []);
+   
+
+
 
 // listen for keyboard input and translate this into game, update the state 
     useEffect(() => {
@@ -103,27 +107,71 @@ export default function GameBoard({userId}: GameBoard) {
             setIsCompleted(true)
         if(sessionId){
             supabase.from("game_sessions").update({status:true, correct_guesses:true}).eq("id", sessionId)
-        }    
-        }  
 
         }
-      
+        // dispatching event to the header. Telling it to update
+        window.dispatchEvent(new Event("wordCompleted"))    
+        }  
 
 
-            
+        }
+        else {
+            const newIncorrectGuesses =[...incorrectGuesses, letter] 
+            setIncorrectGuesses(newIncorrectGuesses)
+            if (newIncorrectGuesses.length >= MAX_GUESSES){
+                setIsFailed(true)
+                if(sessionId){
+            supabase.from("game_sessions").update({status:true, correct_guesses:false}).eq("id", sessionId)
+
+        }
+        // dispatching an event to the header (line 97)
+        window.dispatchEvent(new Event("wordFailed"))   
+            }
+        
+        }      
     }
-    }, [])
+    window.addEventListener("keypress", handleKeyPress)
+    // clean -up by using remove event listener 
+    // using in [] all states that need this useEffect when state is updated this useEffect will run
+    // if empty [] useEffect runs only once on mount. 
+    return ()=> window.removeEventListener("keypress", handleKeyPress)
+    }, [currentWord, isFailed, isCompleted, incorrectGuesses, sessionId, guessedLetters, loading])
 
-    
+    if (error){
+        return (
+            <div>
+                <p>{error}</p>
+                <button onClick={startNewGame}>Try again</button>
+            </div>
+        )
+        
+    }
+    if (!currentWord){
+        return (
+            <div>
+                <p>No words to guess.</p>
+            </div>
+        )
+    }
+    return (
+        <div>
+            <p>{isCompleted? "You guessed the word!": isFailed?"That's not the word.": "Press the button to play again"}</p>
+            <WordCard word={currentWord.word} guessedLetters={isFailed?new Set(currentWord.word.toLowerCase().split('')):guessedLetters}/>
+            {!isCompleted && !isFailed && <WordMeaning meaning={currentWord.meaning}/>}
+            {incorrectGuesses.length > 0 && <div>
+                <p>Incorrect Guesses: {incorrectGuesses.length}/{MAX_GUESSES}</p>
+                <p>You tried letters: {incorrectGuesses.join(", ").toUpperCase()}</p>
+                </div>}
+            {isCompleted || isFailed && <CompleteDisplay word={currentWord.word} meaning={currentWord.meaning} examples={currentWord.examples} failed={isFailed}/>}    
+            <div>
+               <button onClick={startNewGame}>{isComplete || isFailed? "Start a new game": "Skip this word"}</button> 
+            </div>
+        </div>
+    )
+
 }
 
-    
-//     function isComplete(word: string, guessed: string[]) {
-//         return word
-//            .toLowerCase()
-//            .split('')
-//            .every(letter => guessed.includes(letter))
-// }    
+
 
     
         
